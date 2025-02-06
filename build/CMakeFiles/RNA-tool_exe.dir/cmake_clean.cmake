58ae59c3509b88870dbe43120b5138c40752df2e
kayjay-is-here <yoko.nasana@gmail.com>
2025-02-06 13:26:33 -0800

    Added compile script

---
+++ build/CMakeFiles/RNA-tool_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RNA-tool_exe.dir/link.d"
+  "CMakeFiles/RNA-tool_exe.dir/source/main.cpp.o"
+  "CMakeFiles/RNA-tool_exe.dir/source/main.cpp.o.d"
+  "RNA-tool"
+  "RNA-tool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RNA-tool_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/RNA-tool_lib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RNA-tool_lib.
+# This may be replaced when dependencies are built.
